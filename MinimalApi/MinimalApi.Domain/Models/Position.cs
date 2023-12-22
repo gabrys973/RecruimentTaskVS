b80192dcecd0ee097249d0d2867b4f14abbb6a21
Gabriel Sidorek <g.sidorek@american-systems.pl>
2023-12-22 10:40:03 +0100

    feat: add cores and tests projects

---
+++ MinimalApi/MinimalApi.Domain/Models/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace MinimalApi.Domain.Models;
+
+public record Position
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public int Amount { get; set; }
+    public decimal NetPrice { get; set; }
+    public decimal NetWorth { get; set; }
+    public decimal Vat { get; set; }
+    public int AmountBefore { get; set; }
+    public decimal AvgBefore { get; set; }
+    public int AmountAfter { get; set; }
+    public decimal AvgAfter { get; set; }
+    public string Group { get; set; }
+}
